c092d913fa88b449115286de437e8e2c3d10aed2
Nirajan Acharya <87110903+nirajanacharya@users.noreply.github.com>
2024-10-04 01:14:41 +0000

    commit message

--- scripts.js
+++ scripts.js
@@ -1,4 +1,4 @@
-// Ensure the DOM is fully loaded
+
 document.addEventListener('DOMContentLoaded', function() {
     // Function to switch languages
     function setLanguage(language) {
@@ -25,12 +25,12 @@ document.addEventListener('DOMContentLoaded', function() {
     }
 
     // Add click event to each phone link
-    const phones = document.querySelectorAll('.phone');
-    phones.forEach(function(phone) {
-        phone.addEventListener('click', function() {
-            alert('आपको आपतकालीन सेवामा फोन गरिँदैछ।');
-        });
-    });
+    // const phones = document.querySelectorAll('.phone');
+    // phones.forEach(function(phone) {
+    //     phone.addEventListener('click', function() {
+    //         alert('');
+    //     });
+    // });
 
     // Expose setLanguage to global scope so it can be called from HTML
     window.setLanguage = setLanguage;
